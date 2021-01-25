72f67acb264694dcf8c04de256f4e9480ffd3807
greencee <chgreen@microsoft.com>
2021-01-25 14:18:09 +0000

    Added USER variable explicitly

--- accumulo-docker-build.sh
+++ accumulo-docker-build.sh
@@ -34,12 +34,17 @@ RUN useradd -g ${GROUP_ID} -u ${USER_ID} -k /root -m ${USER_NAME}
 RUN mkdir -p /etc/sudoers.d
 RUN echo "${USER_NAME} ALL=NOPASSWD: ALL" > "/etc/sudoers.d/accumulo-build-${USER_ID}"
 ENV HOME /home/${USER_NAME}
+ENV USER ${USER_NAME}
 UserSpecificDocker
+
+#If this env varible is empty, docker will be started	#If this env varible is true, docker will be started
+# in non interactive mode	# in interactive mode
+DOCKER_INTERACTIVE_RUN=${DOCKER_INTERACTIVE_RUN-"-i -t"}
 
 # Run a docker container that has your Accumulo source and Maven repo mapped
 # into the container.  These dirs are mapped into the container using cached,
 # which may cause writes to be slightly delayed but it improves performance.
-docker run -it \
+docker run --rm=true $DOCKER_INTERACTIVE_RUN \
   -v "${ACCUMULO_SRC}:/home/${USER_NAME}/accumulo-src:cached" \
   -w "/home/${USER_NAME}/accumulo-src" \
   -v "${HOME}/.m2:/home/${USER_NAME}/.m2:cached" \
